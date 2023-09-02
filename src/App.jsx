fe2f21bb27c37308856a43c3e66766a97c8dc0a2
rigatalarcon <ricardo.alarcon82@gmail.com>
2023-09-02 02:22:34 -0300

    iniciando otra vez

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { useState } from 'react'
+import Navbar from './componets/Navbar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<ItemListContainer greeting={'Listado de todos los productos'}/> }/>
+          <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Productos por categoria'}/>} />
+          <Route path='/detail/:productId' element={<ItemDetailContainer />} />
+          <Route path='*' element={<h1>Error 404 Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
+
