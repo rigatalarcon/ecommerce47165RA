2ec85138efae046142bee0755bdddf93c54ee27f
rigatalarcon <ricardo.alarcon82@gmail.com>
2023-09-12 12:00:40 -0300

    re agregado de notifcation, la 1 estaba mal

--- src/Notification/NotificationService.jsx
+++ src/Notification/NotificationService.jsx
@@ -1,104 +1,48 @@
-import { useState } from 'react'
-import { useCart } from '../../context/CartContext'
-import { useNotification } from '../../notification/NotificationService'
+import { useState, createContext, useContext } from "react"
 
-const InputCount = ({ onAdd, stock, initial }) => {
-    const [count, setCount] = useState(initial)
-
-    const handleChange = (e) => {
-        if(e.target.value <= stock) {
-            setCount(e.target.value)
-        }
+const Notification = ({ data }) => {
+    const notificationStyle = {
+        position: 'absolute',
+        top: 100,
+        right: 30,
+        backgroundColor: data.type === 'success' ? 'green' : 'red',
+        color: 'white',
+        padding: '10px 20px 10px 20px',
+        fontSize: 20,
+        borderRadius: 10
     }
 
     return (
-        <div>
-            <input type='number' onChange={handleChange} value={count}/>
-            <button onClick={() => onAdd(count)}>Agregar al carrito</button>
+        <div style={notificationStyle}>
+            {data.text}
         </div>
     )
 }
 
-const ButtonCount = ({ onAdd, stock, initial = 1 }) => {
-    const [count, setCount] = useState(initial)
+const NotificationContext = createContext()
 
-    const increment = () => {
-        if(count < stock) {
-            setCount(count + 1)
-        }
+export const NotificationProvider = ({ children }) => {
+    const [notificationData, setNotificationData] = useState({
+        type: 'success',
+        text: ''
+    })
 
-    }
-
-    const decrement = () => {
-            setCount(count - 1)
+    const setNotification = (type, text, time = 2) => {
+        setNotificationData({ type, text })
+        setTimeout(() => {
+            setNotificationData(prev => { return { ...prev, text: '' } })
+        }, (time * 1000))
     }
 
     return (
-        <div>
-            <p>{count}</p>
-            <button onClick={decrement}>-</button>
-            <button onClick={increment}>+</button>
-            <button onClick={() => onAdd(count)}>Agregar al carrito</button>
-        </div>
+        <NotificationContext.Provider value={{ setNotification }}>
+            {notificationData.text && <Notification data={notificationData} />}
+            {children}
+        </NotificationContext.Provider>
     )
 }
 
 
-const ItemDetail = ({ id, name, category, img, price, stock, description }) => {
-    const [inputType, setInputType] = useState('button')
-    const [quantity, setQuantity] = useState(0)
-
-    const ItemCount = inputType === 'input' ? InputCount : ButtonCount
-
-    const { addItem } = useCart()
-    const { setNotification } = useNotification()
-
-    const handleOnAdd = (quantity) => {
-        const objProductToAdd = {
-            id, name, price, quantity
-        }
-        addItem(objProductToAdd)
-        setNotification('error', `Se agregaron correctamente ${quantity} ${name} al carrito`)
-        console.log('agregue al carrito: ', quantity)
-
-        setQuantity(quantity)
-    }
-
-    return (
-        <article>
-            <button onClick={() => setInputType(inputType === 'input' ? 'button' : 'input')}>
-                Cambiar contador
-            </button>
-            <header>
-                <h2>
-                    {name}
-                </h2>
-            </header>
-            <picture>
-                <img src={img} alt={name} style={{ width: 100}}/>
-            </picture>
-            <section>
-                <p>
-                    Categoria: {category}
-                </p>
-                <p>
-                    Descripción: {description}
-                </p>
-                <p>
-                    Precio: {price}
-                </p>
-            </section>           
-            <footer>
-                {
-                    quantity === 0 ? (
-                        <ItemCount onAdd={handleOnAdd} stock={stock}/>
-                    ) : (
-                        <button>Finalizar compra</button>
-                    )
-                }
-            </footer>
-        </article>
-    )
+export const useNotification = () => {
+    return useContext(NotificationContext)
 }
-
-export default ItemDetail
